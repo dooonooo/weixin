b5142df9658b6200bfcd5d8d7e1d1b1f52eb6c68
lidong <lidongnet9@gmail.com>
2014-09-10 01:32:38 +0800

    添加笑话支持，笑话cvs文件

---
+++ xiaohua.php
@@ -0,0 +1,22 @@
+<?php
+header("Content-type: text/html; charset=utf-8"); 
+
+
+$con = mysql_connect("localhost","root","") or die(mysql_error());;
+
+mysql_select_db("weixin", $con);
+mysql_query("SET NAMES UTF8;"); //设置编码
+
+//$num=mt_rand(min,max)
+$db_xiao_count=mt_rand(1,137);
+//$sql = "SELECT * FROM `xiaohua` WHERE `id` = 2
+
+$result = mysql_query("SELECT * FROM `xiaohua` WHERE `id`=$db_xiao_count");
+
+$row = mysql_fetch_array($result);
+  {
+  echo $row['title'] . $row['content'];
+  echo "<br />";
+  }
+
+mysql_close($con);
